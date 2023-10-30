065eb545b5caa0d15c0387acf56d507a9fd64f78
Cem Okulmus <36513234+cem-okulmus@users.noreply.github.com>
2023-10-30 16:18:13 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cem-okulmus/npqgen
+
+go 1.20
---
+++ npqgen.go
@@ -0,0 +1,12 @@
+package main
+
+/*   Navigational Path Query Generator
+     ---------------------------------
+
+     - This tool takes as input a schema-like structure that represents a graph-database
+       and generates queries that represent randomly generated paths through the above schema.
+
+*/
+
+func main() {
+}
